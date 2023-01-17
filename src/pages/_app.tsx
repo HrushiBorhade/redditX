8998d002e252172cf5e8bf4183f0784f2fe2b3b3
Hrushikesh Borhade <hrushiborhade123@gmail.com>
2023-01-17 19:20:13 +0530

    initialised our next application, core package installations , chakra theme,fonts and global styling setup

---
+++ src/pages/_app.tsx
@@ -0,0 +1,10 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import type { AppProps } from 'next/app'
+import {theme} from "../chakra/theme"
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>)
+}
